9ce0808434fa07dabc730d982a90f0ed06cabd6f
MU FRAMEWORK <127579793+Muframework@users.noreply.github.com>
2023-03-11 11:04:41 +0530

    Add files via upload

---
+++ mu-doc/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+
+Config.Price = 1000
+Config.ReviveTime = 20000  --in msec
+Config.Doctor = 0 -- Minimum Amount of EMS to work
---
+++ mu-doc/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+authors 'MADARA UCHIHA'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+} 
